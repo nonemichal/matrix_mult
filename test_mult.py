d7ff5fe86adc30afd91951ae152328d189d69104
Michał Miler <47054736+nonemichal@users.noreply.github.com>
2025-09-07 15:07:52 +0200

    removed comments

--- test_mult.py
+++ test_mult.py
@@ -1,14 +1,11 @@
 import numpy as np
 
-# Load matrices saved from C program
 A = np.load("matrix_a.npy")
 B = np.load("matrix_b.npy")
 C = np.load("matrix_c.npy")
 
-# Multiply in Python
 C_np = A @ B
 
-# Check if they are close
 if np.allclose(C, C_np, atol=1e-12):
     print("The matrices match")
 else:
